c96b426d03e8e557cb3b60deeb2b7131e0cdf9b8
VINCENZO MESCHI <vincenzomeschi12@gmail.com>
2023-12-23 17:14:45 -0500

    Internalize common site assets

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,9 +6,7 @@ import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import Person2OutlinedIcon from "@mui/icons-material/Person2Outlined";
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
-
-const logo =
-	"https://firebasestorage.googleapis.com/v0/b/ruff-3fe3b.appspot.com/o/site_assets%2FRuff_Logo-min.svg?alt=media&token=7df87bf5-37b6-4a80-91cb-fb1b3451b07f";
+import logo from "../../assets/Ruff_Logo-min.svg";
 
 const Navbar = () => {
 	const [user, setUser] = useState(null);
